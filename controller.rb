3e0a8cedba104f00d053256eb5a131d28b3857af
Amarababu277 <158104913+amarababu277@users.noreply.github.com>
2024-04-16 04:19:36 -0500

    routes.rb-prob4_index.html

---
+++ controller.rb
@@ -0,0 +1,19 @@
+class HomeController < ApplicationController
+  def index
+  end
+end
+
+class AboutController < ApplicationController
+  def index
+  end
+end
+
+class WorkController < ApplicationController
+  def index
+  end
+end
+
+class ContactController < ApplicationController
+  def index
+  end
+end
